7a8f4b320bf0c3e5132e001497873ea188fc5f60
Konstantinos Tsiligiannis <panekostas@gmail.com>
2026-05-23 15:24:22 +0300

    Update led.c

--- Bare_metal_embedded/led.c
+++ Bare_metal_embedded/led.c
@@ -6,20 +6,20 @@ void led_init_all(void)
     // RCC AHBENR
     uint32_t *pRccAhbEnr = (uint32_t*)0x40021014;
 
-    // GPIOB MODER - Διορθώθηκε η διεύθυνση εδώ (0x48000400 αντί για 0x48000000)
+    // GPIOB MODER
     uint32_t *pGpioBMode = (uint32_t*)0x48000400;
 
     // enable clock for GPIOB
     *pRccAhbEnr |= (1 << 18);
 
-    // PB3 output mode (καθαρισμός των bit 6,7 και set του bit 6)
+    // PB3 output mode (clear bit 6,7 & set bit 6)
     *pGpioBMode &= ~(3 << (2 * 3));
     *pGpioBMode |=  (1 << (2 * 3));
 }
 
 void led_on(uint8_t led_no)
 {
-    // GPIOB ODR - Αυτό ήταν ήδη σωστό (0x48000400 + 0x14 = 0x48000414)
+    // GPIOB ODR 
     uint32_t *pOdr = (uint32_t*)0x48000414;
     *pOdr |= (1 << led_no);
 }
